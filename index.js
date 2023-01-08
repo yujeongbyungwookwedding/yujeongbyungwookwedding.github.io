7df7bbdc0053412e8d81998c0a2eb51e54843f23
yujeongbyungwookwedding <112526991+yujeongbyungwookwedding@users.noreply.github.com>
2023-01-08 13:38:01 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function copyToClipboard(val) {
+    const t = document.createElement("textarea");
+    document.body.appendChild(t);
+    t.value = val;
+    t.select();
+    document.execCommand('copy');
+    document.body.removeChild(t);
+  }
+  function copy(val) {
+    copyToClipboard(val);
+    alert("복사되었습니다.")
+  }
